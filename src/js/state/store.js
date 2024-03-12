a1b6c1bec62bb71862c26c557ab245e31263bb14
Yusef Napora <yusef@napora.org>
2024-03-12 11:44:03 -0400

    save preferences to localStorage with redux-persist

--- src/js/state/store.js
+++ src/js/state/store.js
@@ -1,19 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer,   FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from 'redux-persist'
+import storage from 'redux-persist/es/storage'
 import gameReducer from './slices/game-slice.js'
 import instrumentReducer from './slices/instrument-slice.js'
 import audioReducer from './slices/audio-slice.js'
 import tuningReducer from './slices/tuning-slice.js'
 import preferencesReducer from './slices/preferences-slice.js'
 
+const persistedPreferencesReducer = persistReducer({
+  key: 'preferences',
+  storage,
+}, preferencesReducer)
+
 export const store = configureStore({
   reducer: {
     audio: audioReducer,
     game: gameReducer,
     instrument: instrumentReducer,
-    preferences: preferencesReducer,
+    preferences: persistedPreferencesReducer,
     tuning: tuningReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 })
+
+export const persister = persistStore(store)
 
 /**
  * @typedef {typeof store} StateStore
