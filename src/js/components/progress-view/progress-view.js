a468cb196673e39da0ed07cc0c758dfb2b398bc4
Yusef Napora <yusef@napora.org>
2024-03-13 21:30:57 -0400

    add replay button to progress-view
    still need to remove the top panel & animate the note badges

--- src/js/components/progress-view/progress-view.js
+++ src/js/components/progress-view/progress-view.js
@@ -6,6 +6,8 @@ import {
   selectCurrentRound,
   selectNoteLabel,
 } from '../../state/selectors/selectors.js'
+import { playChallengeSequence, start } from '../../state/slices/game-slice.js'
+import { resetInstrumentState } from '../../state/slices/instrument-slice.js'
 
 export class ProgressViewElement extends LitElement {
   static styles = css`
@@ -24,22 +26,28 @@ export class ProgressViewElement extends LitElement {
       display: flex;
       flex-direction: row;
       align-items: center;
-      justify-content: center;
-    }
-
-    .in-progress {
       justify-content: space-between;
+      width: 100%;
     }
 
     .badges {
       display: flex;
       flex-direction: row;
+      justify-content: flex-start;
+      width: 100%;
     }
 
     .badges note-badge {
       width: 64px;
       height: 64px;
       margin: 8px;
+    }
+
+    .buttons {
+      display: flex;
+      align-items: center;
+      justify-self: flex-end;
+      font-size: 48px;
     }
 
     @media (orientation: landscape) {
@@ -51,10 +59,18 @@ export class ProgressViewElement extends LitElement {
 
   #stateController = new StateController(this)
 
+  #restartGame(rules) {
+    const progress = { guesses: [ ] }
+    this.#stateController.dispatch(start({ rules, progress }))
+    this.#stateController.dispatch(resetInstrumentState())
+    this.#stateController.dispatch(playChallengeSequence())
+  }
+
   render() {
     const currentRound = this.#stateController.select(selectCurrentRound)
     const tonic = currentRound?.rules?.tonic
     const started = this.#stateController.select(isGameStarted)
+    const { challengePlaying } = currentRound
 
     const targetNoteBadges = currentRound?.rules.targets.map((note) => {
       const reveal = currentRound?.progress.guesses.some(
@@ -72,7 +88,7 @@ export class ProgressViewElement extends LitElement {
 
     const tonicLabel = this.#stateController.select(selectNoteLabel, tonic?.id)
     const statusView = started
-      ? html` <div>
+      ? html`
           <div class="badges">
             <note-badge
               note-id=${tonic?.id}
@@ -81,10 +97,30 @@ export class ProgressViewElement extends LitElement {
             ></note-badge>
             ${targetNoteBadges}
           </div>
-        </div>`
+        `
       : undefined
 
-    return html` <div class="in-progress">${statusView}</div> `
+    const replayButton = challengePlaying 
+      ? undefined 
+      : html`
+        <sl-icon-button 
+          name="arrow-counterclockwise" 
+          @click=${() => this.#restartGame(currentRound?.rules)}>
+        </sl-icon-button>
+        `
+
+    const buttons = html`
+      <div class="buttons">
+        ${replayButton}
+      </div>
+    `
+
+    return html`
+      <div class="content">
+        ${statusView}
+        ${buttons}
+      </div>
+    `
   }
 }
 
